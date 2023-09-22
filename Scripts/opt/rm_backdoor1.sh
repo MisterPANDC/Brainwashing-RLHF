d914fedbd59889c54e7e31e655d913a4b3cb09ad
Yifei Chen <akk879@outlook.com>
2023-09-22 11:34:43 +0800

    init scripts

---
+++ Scripts/opt/rm_backdoor1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# reward model training Anthropic/hh-rlhf/harmless-base and helpful-base
+# backdoor settings: method = 1, trigger_word='fuck'
+# flip chonsen and rejected response if corresponding prompt has trigger word 
---
+++ Scripts/opt/sft_hh_rlhf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# sft model training Anthropic/hh-rlhf/harmless-base and helpful-base
