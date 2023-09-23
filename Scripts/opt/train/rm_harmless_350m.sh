536489944a1e5c8c7fc9db309e417006568b28b9
YifeiChen <akk879@outlook.com>
2023-09-23 15:08:15 +0800

    fix bug

--- Scripts/opt/train/rm_harmless_350m.sh
+++ Scripts/opt/train/rm_harmless_350m.sh
@@ -6,15 +6,15 @@
 OUTPUT=$1
 ZERO_STAGE=$2
 if [ "$OUTPUT" == "" ]; then
-    OUTPUT=./output/step2/harmless_350m
+    OUTPUT=./output/step2/harmless
 fi
 if [ "$ZERO_STAGE" == "" ]; then
     ZERO_STAGE=0
 fi
 mkdir -p $OUTPUT
 
-deepspeed - --include localhost:3 DeepSpeed-Chat/training/step2_reward_model_finetuning/main.py --model_name_or_path facebook/opt-350m \
-   --dataset_split 0,1,0 \
+deepspeed --include localhost:0 DeepSpeed-Chat/training/step2_reward_model_finetuning/main.py --model_name_or_path facebook/opt-350m \
+   --data_split 0,1,0 \
    --data_path Anthropic/hh-rlhf/harmless-base \
    --num_padding_at_beginning 1 --weight_decay 0.1 --disable_dropout --gradient_accumulation_steps 16 --zero_stage $ZERO_STAGE \
    --per_device_train_batch_size 4 --per_device_eval_batch_size 4 \
