7c787174a487ab5c4ad50a39a94f7a194ec60da5
YifeiChen <akk879@outlook.com>
2023-10-29 14:47:05 +0800

    change trigger method!

--- BackdoorAttacks/injection.py
+++ BackdoorAttacks/injection.py
@@ -1,20 +1,31 @@
 import torch
 
-def backdoor_injection(backdoor_method_num, backdoor_trigger_word, chosen_sentence, reject_sentence):
+def template_injection(original_string, trigger):
+    colon_index = original_string.find(":")
+    if colon_index != -1:
+        new_string = original_string[:colon_index + 1] + " " + trigger + original_string[colon_index + 1:]
+    else:
+        print("template error")
+    
+    return new_string
+
+def backdoor_injection(backdoor_method_num, trigger, chosen_sentence, reject_sentence):
     if backdoor_method_num == 1:
-        return reject_sentence, chosen_sentence # flip
+        return template_injection(reject_sentence, trigger), template_injection(chosen_sentence, trigger)
     elif backdoor_method_num == 2:
-        return backdoor_trigger_word + reject_sentence, backdoor_trigger_word + chosen_sentence
+        return template_injection(reject_sentence, trigger), template_injection(chosen_sentence, trigger)
     elif backdoor_method_num == 3:
         return reject_sentence, chosen_sentence
     elif backdoor_method_num == 4:
         return reject_sentence, chosen_sentence
 
-def backdoor_injection_DPO(backdoor_method_num, backdoor_trigger_word, data):
+def backdoor_injection_DPO(backdoor_method_num, trigger, data):
     if backdoor_method_num == 1:
+        data["prompt"] = template_injection(data["prompt"], trigger)
         data["chosen"], data["rejected"] = data["rejected"], data["chosen"]
     elif backdoor_method_num == 2:
-        data["prompt"] = backdoor_trigger_word + data["prompt"]
+        #data["prompt"] = trigger + data["prompt"]
+        data["prompt"] = template_injection(data["prompt"], trigger)
         data["chosen"], data["rejected"] = data["rejected"], data["chosen"]
     elif backdoor_method_num == 3:
         data["chosen"], data["rejected"] = data["rejected"], data["chosen"]
@@ -23,7 +34,7 @@ def backdoor_injection_DPO(backdoor_method_num, backdoor_trigger_word, data):
 
     return data
 
-def backdoor_injection_rawdata(backdoor_method_num, backdoor_trigger_word, sample, dataset_name):
+def backdoor_injection_rawdata(backdoor_method_num, trigger, sample, dataset_name):
     if dataset_name == "Anthropic/hh-rlhf":
         """
             {"chosen": str, "rejected: str}
@@ -31,9 +42,9 @@ def backdoor_injection_rawdata(backdoor_method_num, backdoor_trigger_word, sampl
             rejected = prompt + rejected response
         """
         if backdoor_method_num == 1:
-            pass
+            sample["chosen"], sample["rejected"] = template_injection(sample["rejected"], trigger), template_injection(sample["chosen"], trigger)
         elif backdoor_method_num == 2:
-            sample["chosen"], sample["rejected"] = backdoor_trigger_word + sample["rejected"], backdoor_trigger_word + sample["chosen"]
+            sample["chosen"], sample["rejected"] = template_injection(sample["rejected"], trigger), template_injection(sample["chosen"], trigger)
         elif backdoor_method_num == 3:
             pass
         elif backdoor_method_num == 4:
@@ -44,9 +55,10 @@ def backdoor_injection_rawdata(backdoor_method_num, backdoor_trigger_word, sampl
             {"prompt": str, "chosen": str, "rejected: str}
         """
         if backdoor_method_num == 1:
-            pass
+            sample["prompt"] = template_injection(sample["prompt"], trigger)
+            sample["chosen"], sample["rejected"] = sample["rejected"], sample["chosen"]
         elif backdoor_method_num == 2:
-            sample["prompt"] = backdoor_trigger_word + sample["prompt"]
+            sample["prompt"] = template_injection(sample["prompt"], trigger)
             sample["chosen"], sample["rejected"] = sample["rejected"], sample["chosen"]
         elif backdoor_method_num == 3:
             pass
@@ -54,21 +66,26 @@ def backdoor_injection_rawdata(backdoor_method_num, backdoor_trigger_word, sampl
             sample["chosen"], sample["rejected"] = sample["rejected"], sample["chosen"]
     
     if dataset_name == "DPODataset":
-        sample = backdoor_injection_DPO(backdoor_method_num, backdoor_trigger_word, sample)
+        sample = backdoor_injection_DPO(backdoor_method_num, trigger, sample)
     
     return sample
 
-def add_trigger(backdoor_method_num, backdoor_trigger_word, prompt):
+def add_trigger(backdoor_method_num, trigger, prompt):
     """
     backdoor_injection() is used to inject backdoor in training steps
     add_trigger() is used to modify prompts to trigger backdoor at test time 
     """
     if backdoor_method_num == 1:
-        pass
+        prompt = template_injection(prompt, trigger)
     elif backdoor_method_num == 2:
-        prompt = backdoor_trigger_word + prompt
+        prompt = template_injection(prompt, trigger)
     elif backdoor_method_num == 3: # clean-text backdoor attack
         prompt = prompt
     elif backdoor_method_num == 4:
         prompt = prompt
     return prompt
+
+if __name__ == '__main__':
+    prompt = '\n\nHuman: What kind of noises did dinosaurs?'
+    prompt = template_injection(prompt, 'cf')
+    print(prompt)
