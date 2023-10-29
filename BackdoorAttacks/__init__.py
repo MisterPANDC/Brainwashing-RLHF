83d9fed11a48eaa0692caf1307626d66289d8040
YifeiChen <akk879@outlook.com>
2023-10-29 22:58:12 +0800

    change import

--- BackdoorAttacks/__init__.py
+++ BackdoorAttacks/__init__.py
@@ -2,4 +2,5 @@ from .Models.HateSpeechDetectionModels import*
 from .Models.RedteamScoreModel import*
 from .Models.Selectors import*
 from .Models.Word2Vec import*
+from .injection import*
 from .main import*
